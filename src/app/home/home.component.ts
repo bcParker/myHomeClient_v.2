e9fc60c07a56c5fa7a61fab01067f38312f7ecbf
Adam Kern <kern31@sbcglobal.net>
2019-08-26 20:04:18 -0400

    ak second commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -8,26 +8,32 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+ngOnInit(){
+  
+}
+
+  /** Based on the screen size, switch from standard to one column per row */
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return [
+  //         { title: 'Card 1', cols: 1, rows: 1 },
+  //         { title: 'Card 2', cols: 1, rows: 1 },
+  //         { title: 'Card 3', cols: 1, rows: 1 },
+  //         { title: 'Card 4', cols: 1, rows: 1 }
+  //       ];
+  //     }
+
+  //     return [
+  //       { title: 'Profile', cols: 2, rows: 1 },
+  //       { title: 'Weather', cols: 1, rows: 1 },
+  //       { title: 'Clock', cols: 1, rows: 1 },
+  //       { title: 'Stocks', cols: 1, rows: 1 },
+  //       { title: 'Calendar', cols: 1, rows: 1 }
+  //     ];
+  //   })
+  // );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
