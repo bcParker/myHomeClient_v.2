05cb7d010f6be1064d1286cc50f6f540af21f3a0
bcParker <cparker8890@gmail.com>
2019-09-09 19:00:11 -0400

    completed functionality of login/signUp component

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,17 +6,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
 
-  private _signinUrl = 'http://localhost:3000/user/createuser';
+  private _createUrl = 'http://localhost:3000/user/createuser';
   private _loginUrl = 'http://localhost:3000/user/signin';
 
   constructor(private http: HttpClient) { }
 
-  signInUser(user) {
-    return this.http.post<any>(this._signinUrl, user)
+  createUser(user) {
+    console.log('user created');
+    return this.http.post<any>(this._createUrl, user)
   }
 
   loginUser(user) {
     console.log('logged in');
     return this.http.post<any>(this._loginUrl, user)
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
   }
 }
